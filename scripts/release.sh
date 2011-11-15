3d69913a7acc74e1ddb4e1dd89d5a3bcf45a9e48
Jonas Thiem <contact@eloxoph.com>
2011-11-15 23:38:31 +0100

    More script updates

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,11 +8,12 @@ fi
 rm blitwizard-src.zip
 rm blitwizard-win32.zip
 rm deps.zip
-mkdir tarball
+rm -r tarball/
+mkdir -p tarball
 cd tarball
-git clone http://games.homeofjones.de/blitwizard/git-source/blitwizard.git/ .
+git clone http://games.homeofjones.de/blitwizard/git-source/blitwizard.git/ . || { echo "Failed to do git checkout."; exit 1; }
 rm -rf ./.git
-sh autogen.sh
+sh autogen.sh || { echo "Autoconf generation failed."; exit 1; }
 rm .gitignore
 rm bin/.gitignore
 rm src/sdl/.gitignore
@@ -22,19 +23,21 @@ rm src/lua/.gitignore
 rm src/imgloader/png/.gitignore
 rm src/imgloader/zlib/.gitignore
 cd ..
-mv ./tarball ./blitwizard || { echo "Failed to rename tarball -> blitwizard."; return 1; }
+rm -r blitwizard/
+mv ./tarball ./blitwizard || { echo "Failed to rename tarball -> blitwizard."; exit 1; }
 zip -r -9 ./blitwizard-src.zip ./blitwizard/
 cd blitwizard
-wget http://games.homeofjones.de/blitwizard/deps.zip
+rm deps.zip
+wget http://games.homeofjones.de/blitwizard/deps.zip || { echo "Failed to download deps.zip"; exit 1; }
 unzip deps.zip
 if [ "$TARGETHOST" == "" ]; then
-	./configure
+	./configure || { echo "./configure failed."; exit 1; }
 else
-	./configure --host="$TARGETHOST"
+	./configure --host="$TARGETHOST" || { echo "./configure failed"; exit 1;}
 fi
 make
 cd ..
-mkdir ./blitwizard-bin || { echo "Failed to create blitwizard-bin directory."; return 1; }
+mkdir ./blitwizard-bin || { echo "Failed to create blitwizard-bin directory."; exit 1; }
 cd blitwizard-bin
 mkdir bin
 cp ../blitwizard/src/blitwizard.exe ./bin
