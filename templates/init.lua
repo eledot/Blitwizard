d0f040dd511a206d68283fc2bb071f89d657a7d6
Jonas Thiem <contact@eloxoph.com>
2012-01-06 03:39:58 +0100

    Removing debug print from the template loader

--- templates/init.lua
+++ templates/init.lua
@@ -4,7 +4,6 @@ for index,file in ipairs(os.ls("templates/")) do
 	if os.isdir("templates/" .. file) then
 		local filepath = "templates/" .. file .. "/" .. file .. ".lua"
 		if os.exists(filepath) then
-			print("filepath: " .. filepath)
 			dofile(filepath)
 		end
 	end
