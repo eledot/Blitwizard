8c9c067498c56500c92a674ec1e20fe7f2c12cba
Jonas Thiem <contact@eloxoph.com>
2012-01-31 09:02:33 +0100

    Adding missing Lua include path to Android-blitwizard.mk

--- scripts/android/Android-blitwizard.mk
+++ scripts/android/Android-blitwizard.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../vorbis/include $(LOCAL_PATH)/../ogg/include $(LOCAL_PATH)/../imgloader/ $(LOCAL_PATH)/../box2d/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../vorbis/include $(LOCAL_PATH)/../ogg/include $(LOCAL_PATH)/../imgloader/ $(LOCAL_PATH)/../box2d/ $(LOCAL_PATH)/../lua/
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
