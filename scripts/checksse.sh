00aa2239903a322f6f73fcd5045f83d1d43546ed
Jonas Thiem <contact@eloxoph.com>
2012-02-04 12:01:43 +0100

    Hopefully, this will make the SSE test work more reliably

--- scripts/checksse.sh
+++ scripts/checksse.sh
@@ -8,7 +8,7 @@ EXEEXT=`cat scripts/.buildinfo | grep EXEEXT | sed -e 's/^.*\=//'`
 echo "#include <stdio.h>" > scripts/ssetest.c || { exit 1; }
 echo 'int main() {float a = 1;float b = 2;printf("%f\n",a+b);return 0;}' >> scripts/ssetest.c
 
-$CC -o scripts/ssetest$EXEEXT -msse -msse2 -mfpmath=both scripts/ssetest.c &> /dev/null
+$CC -o scripts/ssetest$EXEEXT -msse -msse2 -mfpmath=both scripts/ssetest.c 2>&1 /dev/null
 
 WINECMD=""
 
@@ -17,8 +17,8 @@ if [ -n "$EXEEXT" ]; then
 #	export WINEDEBUG=-all
 fi
 
-scripts/ssetest$EXEEXT &> /dev/null || {
-	${WINECMD}scripts/ssetest$EXEEXT &> /dev/null || { rm -f scripts/ssetest.c; rm -rf scripts/ssetest$EXEEXT; exit 0; }
+scripts/ssetest$EXEEXT 2>&1 /dev/null || {
+	${WINECMD}scripts/ssetest$EXEEXT 2>&1 /dev/null || { rm -f scripts/ssetest.c; rm -f scripts/ssetest$EXEEXT; exit 0; }
 }
 
 echo " -msse -msse2 -mfpmath=both "
