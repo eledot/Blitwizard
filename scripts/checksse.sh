a5f00b6ace1adaeb1e8813f0abb3801127266ad2
Jonas Thiem <contact@eloxoph.com>
2012-02-04 12:20:42 +0100

    Another shot at that debian ARM compilation problem

--- scripts/checksse.sh
+++ scripts/checksse.sh
@@ -8,7 +8,8 @@ EXEEXT=`cat scripts/.buildinfo | grep EXEEXT | sed -e 's/^.*\=//'`
 echo "#include <stdio.h>" > scripts/ssetest.c || { exit 1; }
 echo 'int main() {float a = 1;float b = 2;printf("%f\n",a+b);return 0;}' >> scripts/ssetest.c
 
-$CC -o scripts/ssetest$EXEEXT -msse -msse2 -mfpmath=both scripts/ssetest.c > /dev/null 2>&1
+errorhappened="no"
+$CC -o scripts/ssetest$EXEEXT -msse -msse2 -mfpmath=both scripts/ssetest.c > /dev/null 2>&1 || { errorhappened="yes" }
 
 WINECMD=""
 
@@ -17,13 +18,21 @@ if [ -n "$EXEEXT" ]; then
 #	export WINEDEBUG=-all
 fi
 
-scripts/ssetest$EXEEXT > /dev/null 2>&1 || {
-	${WINECMD}scripts/ssetest$EXEEXT > /dev/null 2>&1 || { rm -f scripts/ssetest.c; rm -f scripts/ssetest$EXEEXT; exit 0; }
-}
+if [ "$errorhappened" = "no" ]; then
+	scripts/ssetest$EXEEXT > /dev/null 2>&1 || {
+		${WINECMD}scripts/ssetest$EXEEXT > /dev/null 2>&1 || { errorhappened="yes" }
+	}
+fi
+
+if [ "$errorhappened" = "yes" ]; then
+	rm -f scripts/ssetest.c
+	rm -f scripts/ssetest$EXEEXT
+	exit 0
+fi
 
 echo " -msse -msse2 -mfpmath=both "
 
 rm scripts/ssetest.c
 rm scripts/ssetest$EXEEXT
 
-exit 0;
+exit 0
