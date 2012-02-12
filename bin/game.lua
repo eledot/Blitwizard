d5eee1e968f0caaa9ff7b5c48db925d964ab865f
Jonas Thiem <contact@eloxoph.com>
2012-02-12 23:00:29 +0100

    Starting blitwiz.font

--- bin/game.lua
+++ bin/game.lua
@@ -2,10 +2,18 @@
 -- Initialise audio first so FFmpeg is found in case we want it
 pcall(blitwiz.sound.play)
 
+-- Try to run the templates first if they are one folder up
+if os.exists("samplebrowser/browser.lua") then
+	-- templates are indeed one folder up, as it seems
+	local function calltemplates()
+		dofile("../templates/init.lua")
+	end
+	calltemplates()
+end
+
 -- Check if the user created a custom game we would want to run preferrably
 if os.exists("../game.lua") then
 	os.chdir("../")
-	dofile("templates/init.lua")
 	dofile("game.lua")
 	return
 end
--- templates/init.lua
+++ templates/init.lua
@@ -1,5 +1,11 @@
 
---Load all the templates
+-- Avoid double initialisation
+if blitwiz.templatesinitialised == true then
+	return
+end
+blitwiz.templatesinitialised = true
+
+-- Load all the templates
 for index,file in ipairs(os.ls("templates/")) do
 	if os.isdir("templates/" .. file) then
 		local filepath = "templates/" .. file .. "/" .. file .. ".lua"
