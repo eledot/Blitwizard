31e2a45d32a1766f96e046274801cf63cd7dd44b
Jonas Thiem <jonasthiem@googlemail.com>
2012-03-25 21:39:44 +0200

    Documenting things a bit better and fixing cmake test script

--- scripts/checkforcmake.sh
+++ scripts/checkforcmake.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-value="`cmakeargh --help 2> /dev/null | grep 'cmake version'`"
+# This tests whether cmake is present. Returns 0 if yes, otherwise 1
+
+value="`cmake --help 2> /dev/null | grep 'cmake version'`"
 
 if [ -z "$value" ]; then
-    exit 0;
-else
     exit 1;
+else
+    exit 0;
 fi
 
