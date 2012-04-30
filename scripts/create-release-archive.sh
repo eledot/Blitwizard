1f42ea42fb68ce31c256ee97e81b2401dc7e4ca8
Jonas Thiem <jonasthiem@googlemail.com>
2012-04-30 14:41:11 +0200

    Fixing order issue when detecting version by grabbing it from our current source tree instead of the one fetched for the build - that's an incorrect but probably sufficient behaviour for now

--- scripts/create-release-archive.sh
+++ scripts/create-release-archive.sh
@@ -16,7 +16,7 @@ CROSSCCHOST="i686-pc-mingw32"
 # -- End of options --
 
 
-useversion=`grep AC_INIT blitwizard/configure.ac | sed -e "s/AC_INIT[(][[]blitwizard[]], [[]//g" | sed -e "s/[]])//g"`
+useversion=`grep AC_INIT ../configure.ac | sed -e "s/AC_INIT[(][[]blitwizard[]], [[]//g" | sed -e "s/[]])//g"`
 
 #Output some explanations
 echo "This is a release script that builds a blitwizard release tarball."
