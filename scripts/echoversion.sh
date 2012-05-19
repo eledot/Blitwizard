176621bc983408efcb9235fa1f150fb0948da105
Jonas Thiem <jonasthiem@googlemail.com>
2012-05-19 21:01:49 +0200

    Adding proper bash header and a comment to the new script

--- scripts/echoversion.sh
+++ scripts/echoversion.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+# Please run this script with current working directory being "scripts/"
+# - otherwise it won't find the required files.
+
 grep AC_INIT ../configure.ac | sed -e "s/AC_INIT[(][[]blitwizard[]], [[]//g" | sed -e "s/[]])//g"
