cdd35a6709f5e33eb04b73c6c2df4299501c8542
Jonas Thiem <jonasthiem@googlemail.com>
2012-07-05 17:00:56 +0200

    More style checker work

--- scripts/checkstyle.lua
+++ scripts/checkstyle.lua
@@ -2,6 +2,15 @@
 -- Blitwizard style checker
 -- *----------------------*
 
+-- Some style check options:
+
+c_indent_spaces = 4
+c_bracket_on_separate_line = true
+lua_indent_spaces = 4
+lua_use_semicolon = false
+
+-- End of options.
+
 args = {...}
 styleerror = false
 
@@ -33,19 +42,72 @@ function error_shortblockcomment(file, nr)
     print("Style: line " .. file .. ":" .. nr .. " has a short block comment, please convert to line comment(s)")
 end
 
+function error_bracketnotonseparateline(file, nr)
+    styleerror = true
+    print("Style: line " .. file .. ":" .. nr .. " has a bracket on the same line as other code, put it isolated into the next line")
+end
+
+function error_bracketonseparateline(file, nr)
+    styleerror = true
+    print("Style: line " .. file .. ":" .. nr .. " has an isolated bracket, please put it on the previous line")
+end
+
 function checkcommentsandstrings(file, nr, language, line, insideblockcomment, insidestring, blockcommentlength)
-    -- Get start positions of comments, strings:
-    local blockcommentstart = string.find(line, "/*", 1, true)
-    local linecommentstart = string.find(line, "//", 1, true)
+    -- Get start position of block comments:
+    local blockcommentstart = 1/0
+    if language == "c" then
+        local v = string.find(line, "/*", 1, true)
+        if v ~= nil then
+            blockcommentstart = v
+        end
+    elseif language == "lua" then
+        local v = string.find(line, "--[[", 1, true)
+        if v ~= nil then
+            blockcommentstart = v
+        end
+    end
+
+    -- Get start position of line comments:
+    local linecommentstart = 1/0
+    if language == "c" then
+        local v = string.find(line, "//", 1, true)
+        if v ~=  nil then
+            linecommentstart = v
+        end
+    elseif language == "lua" then
+        local v = string.find(line, "--", 1, true)
+        if v ~= nil then
+            if blockcommentstart > v then -- don't interpret as block comment
+                linecommentstart  = v
+            end
+        end
+    end
+
+    -- Get the position of string start:
     local linestringstart = string.find(line, "\"", 1, true)
-    if blockcommentstart == nil then
-        blockcommentstart = 1/0
-    end
-    if linecommentstart == nil then
-        linecommentstart = 1/0
-    end
     if linestringstart == nil then
-        linestringstart = 1/0
+        linestringstart = 1 / 0
+    end
+    -- Skip escaped string starts
+    while linestringstart > 1 and linestringstart < 1 / 0 do
+        if string.sub(line, linestringstart - 1, linestringstart - 1) == "\"" then
+            linestringstart = string.find(line, "\"", linestringstart + 1, true)
+            if linestringstart == nil then
+                linestringstart = 1 / 0
+            end
+        else
+            break
+        end
+    end
+
+    -- Check second string notation type for lua:
+    if language == "lua" then
+        local linestringstart2 = string.find(line, "[[", 1, true)
+        if linestringstart2 ~= nil then
+            if linestringstart2 < linestringstart then
+                linestringstart = linestringstart2
+            end
+        end
     end
 
     -- Now lets go scan for line comments, block comments, strings:
@@ -150,8 +212,28 @@ function checkfile(file)
         -- get line without line, block comments
         local isolatedline = ""
         isolatedline,insideblockcomment,insidestring,blockcommentlength = checkcommentsandstrings(file, n, "c", line, insideblockcomment, insidestring, blockcommentlength)
+        while string.ends(isolatedline, " ") do
+            if #isolatedline > 1 then
+                isolatedline = string.sub(isolatedline, 1, #isolatedline - 1)
+            else
+                isolatedline = ""
+            end
+        end
         if #isolatedline > 0 then
-            --print(file .. ":" .. n .. ": " .. isolatedline)
+            if insidestring == false then
+                if language == "c" then
+                    -- Check for bracket on separate line
+                    if string.ends(isolatedline, "{") then
+                        if c_bracket_on_separate_line then
+                            local linecopy = isolatedline
+                            while string.starts(linecopy, " ") do
+                                linecopy = string.sub(linecopy, 2)
+                            end
+                            if linecopy ~= "{" then
+
+                        end
+                    end
+            end
         end
 
         n = n + 1
