e752efad36a148208a9ce21dfa554197b8e38682
Jonas Thiem <jonasthiem@googlemail.com>
2013-01-30 19:26:26 +0100

    Small autogen update/fix

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-autoreconf -i -f || { echo "Autoconf generation failed! Do you have autotools and autoconf installed?"; exit 1; }
+autoreconf -i -f || { echo "Autoconf generation failed! Do you have automake and autoconf installed? (libtool might also be needed)"; exit 1; }
