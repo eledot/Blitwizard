9d56ea98a41c2b022638245ea5e29cf54423d288
Jonas Thiem <jonasthiem@googlemail.com>
2013-03-07 07:34:03 +0100

    Missing header file for resource.c added

---
+++ src/resources.h
@@ -0,0 +1,67 @@
+
+/* blitwizard game engine - source code file
+
+  Copyright (C) 2013 Jonas Thiem
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+*/
+
+#ifndef BLITWIZARD_RESOURCES_H_
+#define BLITWIZARD_RESOURCES_H_
+
+// The resources code manages all loaded resource zip files
+// and decides where to pick any resource from (image, sound, ...).
+
+// Manage resource zip files:
+int resources_LoadZip(const char* path);
+int resource_LoadZipFromExecutable(const char* path);
+int resource_LoadZipFromOwnExecutable(const char* first_commandline_arg);
+// (all functions return 1 on success, 0 on error)
+
+// Resource location info:
+#define LOCATION_TYPE_ZIP 1
+#define LOCATION_TYPE_DISK 2
+#define MAX_RESOURCE_PATH 100
+struct resourcelocation {
+    int type;  // the location type
+    union {
+        // specified for LOCATION_TYPE_ZIP
+        // (= resource is inside a zip archive)
+        struct {
+            // zip archive the resource is in:
+            struct zipfile* archive;
+            // file path of the file inside the archive:
+            char filepath[MAX_RESOURCE_PATH];
+        } ziplocation;
+        // specified for LOCATION_TYPE_DISK:
+        // (= resource is at a regular hard disk location)
+        struct {
+            // absolute file path to file on disk:
+            char filepath[MAX_RESOURCE_PATH];
+        } disklocation;
+    } location;
+};
+
+// Locate a resource:
+int resource_LocateResource(struct resourcelocation* location);
+// Returns 1 if resource is found, in which case the given
+// resourceinfo struct is modified to share the information.
+// Returns 0 if the resource wasn't found.
+
+#endif  // BLITWIZARD_RESOURCES_H_
+
